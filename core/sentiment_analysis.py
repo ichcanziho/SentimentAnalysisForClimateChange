f86263d5cfd0131da3925e6e50d5dad39074143f
ichcanziho <ichcanzihopi@gmail.com>
2021-05-05 18:48:51 -0500

    clean version

---
+++ core/sentiment_analysis.py
@@ -0,0 +1,142 @@
+import pandas as pd
+from tqdm import tqdm
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from textblob import TextBlob
+import re
+import string
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import unidecode
+from nltk.stem import WordNetLemmatizer
+from nltk import FreqDist
+
+
+class Sentiment:
+    def __init__(self, input_data, output_folder, lower=-0.1, upper=0.1, labels=("neg", "neu", "pos")):
+        self.labels = labels
+        self.input_path = input_data
+        self.save_path = output_folder + "/" + input_data.split("/")[-1].split(".csv")[0] + "_sentiment.csv"
+        self.vader_analyser = SentimentIntensityAnalyzer()
+        self.lower = lower
+        self.upper = upper
+
+    def vader_scores(self, sentence):
+        score = self.vader_analyser.polarity_scores(sentence)
+        score = score['compound']
+        category = self.score_to_categorical(score)
+        return score, category
+
+    def blob_score(self, sentence):
+        score = TextBlob(sentence)
+        score = score.sentiment.polarity
+        category = self.score_to_categorical(score)
+        return score, category
+
+    def score_to_categorical(self, score):
+        if score <= self.lower:
+            return self.labels[0]
+        elif self.lower < score <= self.upper:
+            return self.labels[1]
+        elif self.upper < score <= 1:
+            return self.labels[2]
+        else:
+            return "error"
+
+    def get_line(self, sentence):
+        blob_score, blob_cat = self.blob_score(sentence)
+        vader_score, vader_cat = self.vader_scores(sentence)
+        return [blob_score, vader_score, blob_cat, vader_cat]
+
+    def extract_sentiment_features(self, text_col):
+
+        frame = pd.read_csv(self.input_path)
+        feature_matrix = []
+        for text in tqdm(frame[text_col], desc="extracting sentiments ..."):
+            feature_matrix.append(self.get_line(text))
+
+        syntactic_frame = pd.DataFrame(data=feature_matrix, columns=["blob score", "vader score",
+                                                                     "blob category", "vader category"])
+        frame = pd.concat([frame, syntactic_frame], axis=1)
+        frame.to_csv(self.save_path, index=False)
+
+
+class CriticalWords:
+
+    def __init__(self, input_data, output_folder, text_column, class_column, method_name, n_words=1000):
+
+        self.input_path = input_data
+        self.save_words = f'{output_folder}/critical words/{output_folder.split("/")[-1]}_{method_name}_words.csv'
+        self.save_counts = f'{output_folder}/counts/{output_folder.split("/")[-1]}_{method_name}_counts.csv'
+
+        self.frame = pd.read_csv(self.input_path)
+        self.text_column = text_column
+        self.class_column = class_column
+        self.method_name = method_name
+        self.n_words = n_words
+
+    @staticmethod
+    def clean_sentence(sentence):
+        sentence = sentence.lower()
+        stopwords_english = stopwords.words('english')
+        sentence = re.sub(r'\$\w*', '', sentence)
+        sentence = re.sub(r'^RT[\s]+', '', sentence)
+        sentence = re.sub(r'https?:.*[\r\n]*', '', sentence)
+        sentence = re.sub(r'#', '', sentence)
+        tokens = word_tokenize(text=sentence)
+        sentence_clean = []
+        lemming = WordNetLemmatizer()
+        for word in tokens:
+            if (word not in stopwords_english and  # remove stopwords
+                    word not in string.punctuation and word != 'rt'
+                    and word != '...' and not word.isdigit()
+                    and word.isalpha()):
+                stem_word = lemming.lemmatize(word, pos="v")
+                sentence_clean.append(unidecode.unidecode(stem_word))
+
+        bad_words = ['amp', 'dont', 'doesnt', 'us', 'shes']
+        sentence_clean = [word for word in sentence_clean if word not in bad_words]
+        return sentence_clean
+
+    def generate_list_of_words(self, sentences):
+        words = []
+        for sentence in tqdm(sentences, desc="cleaning sentence"):
+            clean = self.clean_sentence(sentence)
+            words.append(clean)
+        words = sum(words, [])
+        return words
+
+    def extract_most_common_words(self, words, sentiment):
+        word_freq = FreqDist(words)
+        print("for the sentiment", sentiment)
+        print("there are", len(word_freq.keys()), "different words")
+        print("that were used", sum(word_freq.values()), "times")
+        df = pd.DataFrame(
+            {f'{sentiment}_words': list(word_freq.keys()), f'{sentiment}_counts': list(word_freq.values())})
+        df = df.nlargest(self.n_words, columns=f'{sentiment}_counts')
+        df.reset_index(drop=True, inplace=True)
+        return df, len(word_freq.keys()), sum(word_freq.values())
+
+    def extract_words_by_sentiment(self, sentiment):
+        sentiment_words = self.generate_list_of_words(self.frame[self.text_column]
+                                                      [self.frame[self.class_column] == sentiment])
+        sentiment_frame, n_words, n_freq = self.extract_most_common_words(sentiment_words, sentiment)
+        return sentiment_frame, n_words, n_freq
+
+    def extract_critical_words(self):
+
+        frame_base = pd.DataFrame({})
+        n_words, n_freqs, sentiments = [], [], []
+        for sentiment in ['pos', 'neu', 'neg']:
+            sentiment_frame, n_word, n_freq = self.extract_words_by_sentiment(sentiment)
+            n_words.append(n_word)
+            n_freqs.append(n_freq)
+            sentiments.append(sentiment)
+            frame_base = pd.concat([frame_base, sentiment_frame], axis=1)
+        print(frame_base)
+        counts = pd.DataFrame({"sentiment": sentiments, "words": n_words, "freq": n_freqs})
+        s_words = sum(counts['words'])
+        counts['% words'] = counts['words'] / s_words * 100
+        s_freq = sum(counts['freq'])
+        counts['% freq'] = counts['freq'] / s_freq * 100
+        counts.to_csv(self.save_counts, index=False)
+        frame_base.to_csv(self.save_words, index=False)
--- main.py
+++ main.py
@@ -1,21 +1,167 @@
 from core import TwitterCleaner
+from core import NewsCleaner
 from core import Syntactic
-from core import Results
+from core import Sentiment
+from core import CriticalWords
+from core import SummarySentiments
+from core import SummarySyntactic
 
 
 def twitter_clean():
-    twitter = TwitterCleaner(folder="twitter_raw")
-    twitter.hydrate_ids()
+    twitter = TwitterCleaner(input_path="inputs/raw datasets/Twitter",
+                             output_path="outputs/clean datasets/Twitter")
+    twitter.clean_tweets()
 
 
-def extract_syntactic():
-    analyzer = Syntactic("outputs/news/pretty_news_dataset.csv", "outputs/news")
+def news_clean():
+    news = NewsCleaner(input_path="inputs/raw datasets/News/",
+                       output_path="outputs/clean datasets/News")
+    news.clean_news()
+
+
+def twitter_syntactic_analysis():
+    analyzer = Syntactic(input_data="outputs/clean datasets/Twitter/Twitter.csv",
+                         output_folder="outputs/clean datasets/Twitter")
+    analyzer.extract_syntactic_features(text_col="tweet")
+
+
+def news_syntactic_analysis():
+    analyzer = Syntactic(input_data="outputs/clean datasets/News/News.csv",
+                         output_folder="outputs/clean datasets/News")
     analyzer.extract_syntactic_features(text_col="Snippet")
+
+
+def twitter_sentiment_analysis():
+    sentiment_analyzer = Sentiment(input_data="outputs/clean datasets/Twitter/Twitter.csv",
+                                   output_folder="outputs/clean datasets/Twitter")
+    sentiment_analyzer.extract_sentiment_features(text_col="tweet")
+
+    words_analyzer = CriticalWords(input_data="outputs/clean datasets/Twitter/Twitter_sentiment.csv",
+                                   output_folder="outputs/Sentiment Analysis/words/Twitter",
+                                   text_column="tweet",
+                                   class_column="blob category",
+                                   method_name="textBlob",
+                                   n_words=1000)
+    words_analyzer.extract_critical_words()
+
+    words_analyzer = CriticalWords(input_data="outputs/clean datasets/Twitter/Twitter_sentiment.csv",
+                                   output_folder="outputs/Sentiment Analysis/words/Twitter",
+                                   text_column="tweet",
+                                   class_column="vader category",
+                                   method_name="vader",
+                                   n_words=1000)
+    words_analyzer.extract_critical_words()
+
+
+def news_sentiment_analysis():
+    sentiment_analyzer = Sentiment(input_data="outputs/clean datasets/News/News.csv",
+                                   output_folder="outputs/clean datasets/News")
+    sentiment_analyzer.extract_sentiment_features(text_col="Snippet")
+
+    words_analyzer = CriticalWords(input_data="outputs/clean datasets/News/News_sentiment.csv",
+                                   output_folder="outputs/Sentiment Analysis/words/News",
+                                   text_column="Snippet",
+                                   class_column="blob category",
+                                   method_name="textBlob",
+                                   n_words=1000)
+    words_analyzer.extract_critical_words()
+
+    words_analyzer = CriticalWords(input_data="outputs/clean datasets/News/News_sentiment.csv",
+                                   output_folder="outputs/Sentiment Analysis/words/News",
+                                   text_column="Snippet",
+                                   class_column="vader category",
+                                   method_name="vader",
+                                   n_words=1000)
+    words_analyzer.extract_critical_words()
+
+
+def sentiments_graphs():
+    ss = SummarySentiments()
+    color_map = {'pos': 'green', 'neu': 'blue', 'neg': 'red'}
+    ss.make_freq(x_axes_file="outputs/Sentiment Analysis/words/Twitter/critical words/Twitter_vader_words.csv",
+                 y_axes_file="outputs/Sentiment Analysis/words/News/critical words/News_vader_words.csv",
+                 x_max_freq="outputs/Sentiment Analysis/words/Twitter/counts/Twitter_vader_counts.csv",
+                 y_max_freq="outputs/Sentiment Analysis/words/News/counts/News_vader_counts.csv",
+                 sentiments=color_map,
+                 output_dir="outputs/Sentiment Analysis/words/freq distribution/vader_results_10_words.csv",
+                 start=2,
+                 window=10)
+
+    ss.make_freq(x_axes_file="outputs/Sentiment Analysis/words/Twitter/critical words/Twitter_textBlob_words.csv",
+                 y_axes_file="outputs/Sentiment Analysis/words/News/critical words/News_textBlob_words.csv",
+                 x_max_freq="outputs/Sentiment Analysis/words/Twitter/counts/Twitter_textBlob_counts.csv",
+                 y_max_freq="outputs/Sentiment Analysis/words/News/counts/News_textBlob_counts.csv",
+                 sentiments=color_map,
+                 output_dir="outputs/Sentiment Analysis/words/freq distribution/textBlob_results_10_words.csv",
+                 start=2,
+                 window=10)
+
+    ss.plot_sentiment_summary(input_file="outputs/Sentiment Analysis/words/freq distribution/"
+                                         "textBlob_results_10_words.csv",
+                              save_dir="outputs/Sentiment Analysis/images",
+                              classifier='TextBlob',
+                              marker='*',
+                              size=(15, 10),
+                              font_size=12,
+                              ys_lims=(-0.05, 0.2, 0.2, 0.6, 0.6, 1.45),
+                              xs_lims=(-0.05, 0.7, 0.7, 3.0))
+
+    ss.plot_sentiment_summary(input_file="outputs/Sentiment Analysis/words/freq distribution/"
+                                         "vader_results_10_words.csv",
+                              save_dir="outputs/Sentiment Analysis/images",
+                              classifier='Vader',
+                              marker='*',
+                              size=(15, 10),
+                              font_size=12,
+                              ys_lims=(-0.05, 0.4, 0.4, 0.8, 0.8, 1.6),
+                              xs_lims=(-0.05, 1, 1.3, 3.5))
+
+    ss.make_freq(x_axes_file="outputs/Sentiment Analysis/words/Twitter/critical words/Twitter_vader_words.csv",
+                 y_axes_file="outputs/Sentiment Analysis/words/News/critical words/News_vader_words.csv",
+                 x_max_freq="outputs/Sentiment Analysis/words/Twitter/counts/Twitter_vader_counts.csv",
+                 y_max_freq="outputs/Sentiment Analysis/words/News/counts/News_vader_counts.csv",
+                 sentiments=color_map,
+                 output_dir="outputs/Sentiment Analysis/words/freq distribution/vader_results_2_words.csv",
+                 start=0,
+                 window=2)
+
+    ss.make_freq(x_axes_file="outputs/Sentiment Analysis/words/Twitter/critical words/Twitter_textBlob_words.csv",
+                 y_axes_file="outputs/Sentiment Analysis/words/News/critical words/News_textBlob_words.csv",
+                 x_max_freq="outputs/Sentiment Analysis/words/Twitter/counts/Twitter_textBlob_counts.csv",
+                 y_max_freq="outputs/Sentiment Analysis/words/News/counts/News_textBlob_counts.csv",
+                 sentiments=color_map,
+                 output_dir="outputs/Sentiment Analysis/words/freq distribution/textBlob_results_2_words.csv",
+                 start=0,
+                 window=2)
+
+    ss.plot_mini(textBlob="outputs/Sentiment Analysis/words/freq distribution/textBlob_results_2_words.csv",
+                 vader="outputs/Sentiment Analysis/words/freq distribution/vader_results_2_words.csv",
+                 save_dir="outputs/Sentiment Analysis/images/summary.svg",
+                 size=(10, 5))
+
+
+def syntactic_graphs():
+    SummarySyntactic.plot_timelines("syntactic")
+    SummarySyntactic.plot_timelines("sentiment")
 
 
 def main():
 
-    Results.plot_timelines("syntactic")
+    # Cleaning the datasets
+    news_clean()
+    twitter_clean()
+
+    # Making syntactic analysis
+    news_syntactic_analysis()
+    twitter_syntactic_analysis()
+
+    # Making sentiment analysis
+    news_sentiment_analysis()
+    twitter_sentiment_analysis()
+
+    # Plotting the graphs
+    sentiments_graphs()
+    syntactic_graphs()
 
 
 if __name__ == '__main__':
